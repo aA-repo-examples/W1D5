cbba51eb827ea982f3e62c1b1d954eaff8ac374d
jenkennedy <kennifer09@gmail.com>
2018-06-22 14:33:53 -0700

    Create PolyTreeNode class and functionality

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,50 @@
+class PolyTreeNode
+  attr_accessor :children
+  
+  def initialize(value = 0)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+  
+  def parent
+    @parent
+  end
+  
+  def parent=(new_parent)
+    return @parent = nil if new_parent.nil?
+    parent.remove_child(self) if parent
+    @parent = nil
+    new_parent.add_child(self) if new_parent
+    @parent = new_parent
+  end
+  
+  def value
+    @value
+  end
+  
+  def add_child(child)
+    return nil if child.nil?
+    unless children.include? child
+      children << child 
+      child.parent = self
+    end
+  end
+  
+  def remove_child(child)
+    raise "Not a child" unless children.include?(child)
+    
+    children.delete(child)
+    child.parent = nil
+  end
+  
+  def dfs(target_value)
+    return self if target_value == value
+    children.each do |child|
+      node = child.dfs(target_value)
+      return node if node
+    end
+    nil
+  end
+  
+end
