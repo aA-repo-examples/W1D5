c4756f082f44fcb49e3e63f407bc70618f55df04
jenkennedy <kennifer09@gmail.com>
2018-06-22 20:06:13 -0700

    Implement BFS building of path tree & refactor

--- knights_travails/knight.rb
+++ knights_travails/knight.rb
@@ -1,21 +1,56 @@
 require_relative "../skeleton/lib/00_tree_node.rb"
 
 class KnightPathFinder
-  attr_reader :root, :row, :col
+  attr_reader :pos
   
   def initialize(pos)
-    @root = PolyTreeNode.new(pos)
-    @row, @col = pos
-    @visited = []
+    @root  = PolyTreeNode.new(pos)
+    @pos   = pos
+    @seen  = [pos]
+    build_move_tree
   end
+  
+  def find_path(end_pos)
+    trace_path_back(@root.bfs(end_pos))  
+  end
+  
+  def inspect
+    "#<Knight Path Finder with initial position at #{pos}>"
+  end
+  
+  private
+  
+  attr_accessor :seen, :root
+  
+  def trace_path_back(node)
+    return [pos] if node == @root
     
+    trace_path_back(node.parent) << node.value
+  end  
+  
   def new_move_position(pos)
-    new_moves = KnightPathFinder.valid_moves(pos).select do |arr| 
-      row, col = arr
-      (0..7) === row && (0..7) === col && !visited.include?(arr)
+    new_moves = KnightPathFinder.valid_moves(pos)
+      
+    new_moves.select! do |move|
+      !@seen.include?(move) && 
+        move.none?{|el| el > 7 || el < 0}
     end
-    @vistied += new_moves
-    new_moves
+    
+    @seen += new_moves
+    new_moves.map { |move| PolyTreeNode.new(move) }
+  end
+  
+  def build_move_tree
+    queue = [@root]
+    
+    until queue.empty?
+      parent = queue.shift
+      children = new_move_position(parent.value)
+      
+      children.each {|child| child.parent = parent}
+      
+      queue += children
+    end
   end
   
   def self.valid_moves(pos)
@@ -25,4 +60,5 @@ class KnightPathFinder
      [row + 2, col + 1], [row + 2, col - 1],
      [row - 2, col + 1], [row - 2, col - 1]]
   end
+  
 end
